5b0142352710256a719037e572ed5beacdc092f1
ihya_ainnurrohman <ainnurrohman.01@gmail.com>
2021-03-10 12:54:52 +0700

    Initial commit

--- app/src/main/java/org/d3if0004/galerihewan2/MainActivity.kt
+++ app/src/main/java/org/d3if0004/galerihewan2/MainActivity.kt
@@ -1,11 +1,44 @@
 package org.d3if0004.galerihewan2
 
+import Hewan
+import MainAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
+import org.d3if0004.galerihewan2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+//        Log.d("MainActivity", "Jumlah data: " + getData().size)
+        with(binding.recyclerView) {
+            addItemDecoration(DividerItemDecoration(context, RecyclerView.VERTICAL))
+            adapter = MainAdapter(getData())
+            setHasFixedSize(true)
+        }
+    }
+    private fun getData(): List<Hewan> {
+        return listOf(
+            Hewan("Angsa", "Cygnus olor"),
+            Hewan("Ayam", "Gallus gallus"),
+            Hewan("Bebek", "Cairina moschata"),
+            Hewan("Domba", "Ovis ammon"),
+            Hewan("Kalkun", "Meleagris gallopavo"),
+            Hewan("Kambing", "Capricornis sumatrensis"),
+            Hewan("Kelinci", "Oryctolagus cuniculus"),
+            Hewan("Kerbau", "Bubalus bubalis"),
+            Hewan("Kuda", "Equus caballus"),
+            Hewan("Sapi", "Bos taurus"),
+        )
     }
 }
